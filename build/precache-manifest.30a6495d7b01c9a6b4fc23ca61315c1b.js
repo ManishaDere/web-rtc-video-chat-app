1f9faa3cc9328fc4fe42be67995ba1f61783b660
ManishaDere <manu.pathare263@gmail.com>
2020-12-09 11:17:05 +0530

    video chat test first commit

---
+++ build/precache-manifest.30a6495d7b01c9a6b4fc23ca61315c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a56039d8e64178cfeaf6c4512d4113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bbd23e3207a475aefb6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cfe38b4c1e25dcd53e20",
+    "url": "/static/js/2.d3d7cebb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3d7cebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbd23e3207a475aefb6",
+    "url": "/static/js/main.b68e7daa.chunk.js"
+  },
+  {
+    "revision": "0ff731bb10c404f64bf1",
+    "url": "/static/js/runtime-main.d7cd88d6.js"
+  }
+]);
